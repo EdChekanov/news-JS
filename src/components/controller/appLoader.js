d16a45981d360d3ffff6c4486c592b905c89d5f4
EdChekanov <kide.da@gmail.com>
2021-12-05 14:56:46 +0300

    feat: set eslint for ts

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ecaba7c4a29a48b9bbddcadb0a1061f6', // получите свой ключ https://newsapi.org/
         });
     }
 }
